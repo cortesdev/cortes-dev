9d93e2a8b0eb61f9faafc242ccaa22702b37c80a
Ricardo Cortes dos Santos <ricardo.cortesdossantos@diconium.com>
2019-07-24 17:20:00 +0200

    build

---
+++ docs/precache-manifest.eb6f31710665a1e4b4bc8ee220b519b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8695b59182564966bfd7e4db0f52c43a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0aa13c51bca870cb2122",
+    "url": "/static/css/2.35d8b825.chunk.css"
+  },
+  {
+    "revision": "bcfd9ce82df625f2035e",
+    "url": "/static/css/main.e9e0b507.chunk.css"
+  },
+  {
+    "revision": "0aa13c51bca870cb2122",
+    "url": "/static/js/2.dcc16cde.chunk.js"
+  },
+  {
+    "revision": "bcfd9ce82df625f2035e",
+    "url": "/static/js/main.5e766058.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e838ed4fcbc75878c1dfeb650f3892fa",
+    "url": "/static/media/oval.e838ed4f.svg"
+  },
+  {
+    "revision": "6cb0c995f6d8a3f488902298cbdece6d",
+    "url": "/static/media/rightbg.6cb0c995.jpg"
+  },
+  {
+    "revision": "8fad4618b3648854a4f4aacda05a87bf",
+    "url": "/static/media/swift.8fad4618.svg"
+  }
+]);
--- src/components/macbook.js
+++ src/components/macbook.js
@@ -1,37 +1,31 @@
 import React, { Component } from 'react';
 import '../styles/macbook.css';
 
-// this thing is js driven on slid.es so I didn't bother switching out for css animations
-var duration = 7000, steps = 3, step = 1;
-setInterval( function() {
-  document.querySelector( '.animation' ).setAttribute( 'data-animation-step', step = ++step > steps ? 1 : step );
-}, duration / steps );
-
-
+ 
 class Macbook extends Component {
   render() {
     return(
-      <div class="animation" data-animation-step="1">
-        <div class="device">        
-          <div class="phone-home-button"></div>
-          <div class="tablet-home-button"></div>
-          <div className="macbook"> 
-            <div className="macbook-display">
-              <div class="camera"></div>
-              <div class="macbook-screen"></div> 
-                <div className="body"></div>
-              </div>
+      <div className="animation" id="animation" data-animation-step="1">
+      <div className="device">        
+        <div className="phone-home-button"></div>
+        <div className="tablet-home-button"></div>
+        <div className="macbook"> 
+          <div className="macbook-display">
+            <div className="camera"></div>
+            <div className="macbook-screen"></div> 
+              <div className="body"></div>
             </div>
-          <div class="display">
-            <div class="slide1">
-              <div class="macbook-screen__bg"></div>
-              <div>Recent <em>frameworks</em></div>
-            </div> 
-            <div class="slide2"><div> Progressive  <em>enhancement</em></div></div>
-            <div class="slide3"><div>React native <em>Apps</em></div></div>
           </div>
+        <div className="display">
+          <div className="slide1">
+            <div className="macbook-screen__bg"></div>
+            <div>Recent <em>frameworks</em></div>
+          </div> 
+          <div className="slide2"><div> Progressive  <em>enhancement</em></div></div>
+          <div className="slide3"><div>React native <em>Apps</em></div></div>
         </div>
-	  	</div>    
+      </div>
+    </div>    
     )
   }
 }
